9527bf8f2a23e350bcc9bd5d731f1109aa27749d
Walter Banda <walterbanda98@gmail.com>
2025-08-10 14:17:56 +0200

    Add initial environment configuration files and a PNG asset for the top navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'eskalate-meals';
+
+  @ViewChild('addMealDialog') addMealDialog!: ElementRef<HTMLDialogElement>;
+
+  openAddMealDialog() {
+    this.addMealDialog.nativeElement.showModal();
+  }
+
+  closeAddMealDialog() {
+    this.addMealDialog.nativeElement.close();
+  }
+
+  onSubmitMeal(event: Event) {
+    event.preventDefault();
+    // Handle form submission here
+    console.log('Meal submitted!');
+    this.closeAddMealDialog();
+  }
 }
